4dd9caf61d0621771423e9cde52bd3ae429ec0fa
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2023-02-17 22:07:35 -0300

    feat: initialize sdk script

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,33 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "") {
+  console.log("🛑 Private key not found.");
+}
+
+if (!process.env.QUICKNODE_API_URL || process.env.QUICKNODE_API_URL === "") {
+  console.log("🛑 QuickNode API URL not found.");
+}
+
+if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS === "") {
+  console.log("🛑 Wallet Address not found.");
+}
+
+const sdk = ThirdwebSDK.fromPrivateKey(
+  process.env.PRIVATE_KEY,
+  process.env.QUICKNODE_API_URL,
+);
+
+(async () => {
+  try {
+    const address = await sdk.getSigner().getAddress();
+    console.log("🔑 Signer address:", address);
+  } catch (err) {
+    console.log("🛑 Error getting signer address:", err);
+    process.exit(1);
+  }
+})()
+
+export default sdk;

bb117b1ade12feff17ab322da727515867b26452
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2023-02-22 01:28:58 -0300

    feat: create vote proposals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useAddress, ConnectWallet, Web3Button, useContract, useNFTBalance } from '@thirdweb-dev/react'
 import { useState, useEffect, useMemo } from 'react';
+import { AddressZero } from "@ethersproject/constants";
+
 
 const App = () => {
   const address = useAddress();
@@ -7,8 +9,10 @@ const App = () => {
 
   const editionDropAddress = process.env.REACT_APP_EDITION_DROP_CONTRACT_ADDRESS;
   const tokenAddress = process.env.REACT_APP_TOKEN_CONTRACT_ADDRESS;
+  const voteAddress = process.env.REACT_APP_VOTE_CONTRACT_ADDRESS;
   const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
   const { contract: token } = useContract(tokenAddress, "token");
+  const { contract: vote } = useContract(voteAddress, "vote");
   const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
 
   const hasClaimedNFT = useMemo(() => {
@@ -21,6 +25,53 @@ const App = () => {
   const shortenAddress = (str) => {
     return str.substring(0, 6) + '...' + str.substring(str.length - 4);
   };
+
+  const [proposals, setProposals] = useState([]);
+  const [isVoting, setIsVoting] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
+
+  useEffect(() => {
+    if (!hasClaimedNFT) { 
+      return;
+    }
+
+    const getAllProposals = async () => {
+      try {
+        const proposals = await vote.getAll();
+        setProposals(proposals);
+        console.log('📃 Proposals', proposals);
+      } catch (error) {
+        console.error('failed to get proposals', error);
+      }
+    }
+
+    getAllProposals();
+  }, [hasClaimedNFT, vote]);
+
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    if (!proposals.length) {
+      return;
+    }
+    
+    const checkIfUserHasVoted = async () => {
+      try {
+        const hasVoted = await vote.hasVoted(proposals[0].proposalId, address);
+        setHasVoted(hasVoted);
+        if (hasVoted) {
+          console.log("🥵 User has already voted");
+        } else {
+          console.log("🙂 User has not voted yet");
+        }
+      } catch (error) {
+        console.error("Failed to check if wallet has voted", error);
+      }
+    }
+    checkIfUserHasVoted();
+  }, [hasClaimedNFT, proposals, address, vote]);
 
   useEffect(() => {
     if (!hasClaimedNFT) {
@@ -80,7 +131,7 @@ const App = () => {
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1>🏛️ AgoraDAO Member Page</h1>
+      <h1>🏛️ AgoraDAO Member Page</h1>
         <p>Congratulations on being a member</p>
         <div>
           <div>
@@ -104,10 +155,110 @@ const App = () => {
               </tbody>
             </table>
           </div>
+          <div>
+            <h2>Active Proposals</h2>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setIsVoting(true);
+                const votes = proposals.map((proposal) => {
+                  const voteResult = {
+                    proposalId: proposal.proposalId,
+                    vote: 2,
+                  };
+                  proposal.votes.forEach((vote) => {
+                    const elem = document.getElementById(
+                      proposal.proposalId + '-' + vote.type,
+                    );
+
+                    if (elem.checked) {
+                      voteResult.vote = vote.type;
+                      return;
+                    }
+                  });
+                  return voteResult;
+                });
+
+                try {
+                  const delegation = await token.getDelegationOf(address);
+                  if (delegation === AddressZero) {
+                    await token.delegateTo(address);
+                  }
+                  try {
+                    await Promise.all(
+                      votes.map(async ({ proposalId, vote: _vote }) => {
+                        const proposal = await vote.get(proposalId);
+                        if (proposal.state === 1) {
+                          return vote.vote(proposalId, _vote);
+                        }
+                        return;
+                      }),
+                    );
+                    try {
+                      await Promise.all(
+                        votes.map(async ({ proposalId }) => {
+                          const proposal = await vote.get(proposalId);
+                          if (proposal.state === 4) {
+                            return vote.execute(proposalId);
+                          }
+                        }),
+                      );
+                      setHasVoted(true);
+                      console.log('successfully voted');
+                    } catch (err) {
+                      console.error('failed to execute votes', err);
+                    }
+                  } catch (err) {
+                    console.error('failed to vote', err);
+                  }
+                } catch (err) {
+                  console.error('failed to delegate tokens');
+                } finally {
+                  setIsVoting(false);
+                }
+              }}
+            >
+              {proposals.map((proposal) => (
+                <div key={proposal.proposalId} className="card">
+                  <h5>{proposal.description}</h5>
+                  <div>
+                    {proposal.votes.map(({ type, label }) => (
+                      <div key={type}>
+                        <input
+                          type="radio"
+                          id={proposal.proposalId + '-' + type}
+                          name={proposal.proposalId}
+                          value={type}
+                          defaultChecked={type === 2}
+                        />
+                        <label htmlFor={proposal.proposalId + '-' + type}>
+                          {label}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+              <button disabled={isVoting || hasVoted} type="submit">
+                {isVoting
+                  ? 'Voting...'
+                  : hasVoted
+                  ? 'You Already Voted'
+                  : 'Submit Votes'}
+              </button>
+              {!hasVoted && (
+                <small>
+                  This will trigger multiple transactions that you will need to
+                  sign.
+                </small>
+              )}
+            </form>
+          </div>
         </div>
       </div>
     );
-  }  
+  }
 
   return (
     <div className="mint-nft">
